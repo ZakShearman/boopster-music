e502e78342766101997d4d9e06d139735d023bdb
Zak <shearmanzak@gmail.com>
2022-06-18 00:10:40 +0100

    Fix env variable name

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,10 @@ plugins {
 val versionObject = Version(major = "1", minor = "0", revision = "0")
 
 fun buildNumber(): String? {
-    return System.getenv("build.number") ?: System.getProperty("build.number")
+    return System.getenv("system.build.number") ?: System.getProperty("system.build.number")
 }
-println("Build Number A " + System.getenv("build.number"))
-println("Build Number B " + System.getProperty("build.number"))
+println("Build Number A " + System.getenv("system.build.number"))
+println("Build Number B " + System.getProperty("system.build.number"))
 println("Build Number C " + buildNumber())
 
 project.version = "$versionObject" + if (buildNumber() != null) "_" + buildNumber() else ""
